8847fe98405614eb62875ba1153c020be3d63759
pwnwriter <hey@pwnwriter.xyz>
2022-01-01 12:00:00 -0500

    feat(init): initial project skeleton & basic structure

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "Run commands to a server via ssh";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+
+      darwinDeps =
+        pkgs:
+        with pkgs;
+        with pkgs.darwin.apple_sdk.frameworks;
+        [
+          Cocoa
+          libiconv
+        ];
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          name = "sshy";
+          packages =
+            (with pkgs; [
+              cmake
+              # rustc
+              # cargo
+            ])
+            ++ (pkgs.lib.optional pkgs.stdenvNoCC.isDarwin (darwinDeps pkgs));
+        };
+      });
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+      packages = forAllSystems (pkgs: {
+        sshy =
+          with pkgs;
+          let
+            fs = lib.fileset;
+            sourceFiles = fs.unions [
+              ./Cargo.lock
+              ./Cargo.toml
+              ./src
+            ];
+
+            cargoToml = with builtins; (fromTOML (readFile ./Cargo.toml));
+            pname = cargoToml.package.name;
+            version = cargoToml.package.version;
+            cargoLock.lockFile = ./Cargo.lock;
+            darwinBuildInputs = (darwinDeps pkgs);
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            inherit pname version cargoLock;
+            src = fs.toSource {
+              root = ./.;
+              fileset = sourceFiles;
+            };
+            nativeBuildInputs = [
+              clippy
+              rustfmt
+              openssl
+              pkg-config
+              cmake
+              git
+            ];
+            buildInputs = lib.optionals stdenv.isDarwin darwinBuildInputs;
+            preBuildPhases = [ "cargoFmt" ];
+            cargoFmt = ''
+              cargo fmt --manifest-path ./Cargo.toml --all --check
+            '';
+            preInstallPhases = [ "clippy" ];
+            clippy = ''
+              cargo clippy -- --deny warnings
+            '';
+          };
+        default = self.packages.${pkgs.system}.sshy;
+      });
+      apps = forAllSystems (pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.sshy}/bin/sshy";
+        };
+      });
+    };
+}
