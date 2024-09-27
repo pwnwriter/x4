bfa807fb6f754a6880af63994ed4caf03e3e1a96
pwnwriter <hey@pwnwriter.xyz>
2024-09-27 17:11:22 -0400

    feat(exec): run app via different entrypoint

--- src/exec.rs
+++ src/exec.rs
@@ -1,3 +1,32 @@
-#[allow(dead_code)]
+use miette::{Context, Result};
+use sxm::corex::{args::Cli, inspect, parser::Pipeline, ssh};
 
-fn print_soemthing() {}
+pub fn run_app(pipeline: Pipeline, cli: Cli) -> Result<()> {
+    for server in pipeline.servers {
+        println!("{:?}", server.name);
+        println!("{:?}", server.commands[1]);
+
+        let private_key_path = server
+            .resolve_private_key()
+            .context("Failed to resolve private key")?;
+        println!("{:?}", private_key_path);
+
+        let password = server.resolve_password();
+
+        println!("{:?}", password);
+
+        println!("{}", server.port);
+    }
+
+    if cli.inspect {
+        inspect::inspect_available();
+    }
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+
+    ssh::connect_via_password("44.202.26.95".to_string(), "fawn".to_string(), Some(22));
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,21 @@ mod corex;
 mod exec;
 
 use clap::Parser;
-use corex::parser::parse_pipeline;
-use corex::{args, inspect, ssh};
 use miette::{Context, Result};
 use std::path::Path;
+use sxm::args;
+use sxm::parser::parse_pipeline;
 
 fn main() -> Result<()> {
     let cli = args::Cli::parse();
-    let pipeline_file_path = cli.pipeline;
+
+    let pipeline_file_path = cli.pipeline.clone();
+
     let path = Path::new(&pipeline_file_path);
+
     let pipeline = parse_pipeline(path).context("Pipeline parsing failed")?;
 
-    for server in pipeline.servers {
-        println!("{:?}", server.name);
-        println!("{:?}", server.commands[1]);
+    exec::run_app(pipeline, cli)?;
 
-        let private_key_path = server
-            .resolve_private_key()
-            .context("Failed to resolve private key")?;
-        println!("{:?}", private_key_path);
-
-        let password = server.resolve_password();
-
-        println!("{:?}", password);
-
-        println!("{}", server.port);
-    }
-
-    if cli.inspect {
-        inspect::inspect_available();
-    }
-
-    // tracing_subscriber::fmt()
-    //     .with_max_level(tracing::Level::TRACE)
-    //     .init();
-
-    ssh::connect_via_password("44.202.26.95".to_string(), "fawn".to_string(), Some(22));
     Ok(())
 }
